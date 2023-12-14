a8ee74d679055a0cc006b8a28613b0208bc9289f
Asma22334585 <guesmihadil0@gmail.com>
2023-12-14 11:11:45 -0800

    my commit

--- push.c
+++ push.c
@@ -5,8 +5,8 @@
  * push_e - pushes an element to the stack.
  * @new_node: Pointer
  * @l_nmb: Int
- * Description: Adds a node to the stack
  */
+
 void push_e(stack_t **new_node, __attribute__((unused))unsigned int l_nmb)
 {
 	stack_t *tmp;
@@ -29,8 +29,8 @@ void push_e(stack_t **new_node, __attribute__((unused))unsigned int l_nmb)
  * starting from the top of the stack.
  * @stack: Pointer
  * @l_nmb: line number
- * Description: prints all the values on the stack
  */
+
 void print_all_v(stack_t **stack, __attribute__((unused))unsigned int l_nmb)
 {
 	stack_t *tmp;
